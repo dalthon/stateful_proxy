ee7e080a50510ded42d9b43893b662ad25657afc
Dalton Pinto <dalton.teixeira@stone.com.br>
2023-10-17 20:10:12 +0200

    Make example works without no implementation

--- examples/01-simple.go
+++ examples/01-simple.go
@@ -1,22 +1,107 @@
 package main
 
 import (
+	"context"
 	"fmt"
+	"io"
+	"net/http"
+	"net/http/httputil"
+	"net/url"
+	"os"
+	"strings"
 	"time"
 
-	redis "github.com/go-redis/redis"
+	redis "github.com/redis/go-redis/v9"
 )
 
-func main() {
+type HandlerFunc = func(http.ResponseWriter, *http.Request)
+
+type StatefulProxy struct {
+	cluster *redis.ClusterClient
+	url     *url.URL
+}
+
+func New() *StatefulProxy {
 	cluster := redis.NewClusterClient(&redis.ClusterOptions{
-		Addrs: []string{"redis:10001", "redis:10002", "redis:10003"},
+		Addrs:    strings.Split(os.Getenv("REDIS_URLS"), ","),
+		Username: os.Getenv("REDIS_USERNAME"),
+		Password: os.Getenv("REDIS_PASSWORD"),
+		PoolSize: 20,
 	})
 
-	cluster.Set("123", "456", 20*time.Second).Result()
-	result, err := cluster.Get("123").Result()
+	url, err := url.Parse("http://" + os.Getenv("HOSTNAME") + ":3000")
 	if err != nil {
 		panic(err)
 	}
 
-	fmt.Println("Result:", result)
+	return &StatefulProxy{
+		cluster: cluster,
+		url:     url,
+	}
+}
+
+func (proxy *StatefulProxy) Middleware(handleFunc HandlerFunc, duration time.Duration) HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		partitionKey := r.Header.Get("X-Partition-Key")
+
+		thisUrl := proxy.url.String()
+		args := redis.SetArgs{
+			Mode:    "NX",
+			TTL:     duration,
+			Get:     true,
+			KeepTTL: false,
+		}
+
+		ctx := context.Background()
+		url, err := proxy.cluster.SetArgs(ctx, partitionKey, thisUrl, args).Result()
+		if err != nil && err != redis.Nil {
+			panic(err)
+		}
+
+		if url == thisUrl || err == redis.Nil {
+			proxy.cluster.ExpireGT(ctx, partitionKey, duration)
+			handleFunc(w, r)
+			proxy.cluster.Del(ctx, partitionKey)
+			return
+		}
+
+		proxy.remoteCall(url, w, r)
+	}
+}
+
+func (proxy *StatefulProxy) remoteCall(stringURL string, w http.ResponseWriter, r *http.Request) {
+	remoteURL, err := url.Parse(stringURL)
+	if err != nil {
+		panic(err)
+	}
+	remoteProxy := httputil.NewSingleHostReverseProxy(remoteURL)
+
+	r.Host = remoteURL.Host
+	remoteProxy.ServeHTTP(w, r)
+}
+
+func main() {
+	proxy := New()
+
+	duration := 10 * time.Second
+
+	http.HandleFunc("/", proxy.Middleware(getRoot, duration))
+	http.HandleFunc("/hello", proxy.Middleware(getHello, duration))
+
+	fmt.Println("Starting...")
+	if err := http.ListenAndServe(":3000", nil); err != nil {
+		panic(err)
+	}
+
+	fmt.Println("Finished!")
+}
+
+func getRoot(w http.ResponseWriter, r *http.Request) {
+	fmt.Printf("got / request\n")
+	io.WriteString(w, "This is my website!\n")
+}
+
+func getHello(w http.ResponseWriter, r *http.Request) {
+	fmt.Printf("got /hello request\n")
+	io.WriteString(w, "Hello, HTTP!\n")
 }
